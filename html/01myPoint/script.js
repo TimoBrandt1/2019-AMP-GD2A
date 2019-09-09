d7280d7bd372ca7b49af86fff4b495af57aee0c1
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2019-09-09 14:56:20 +0200

    Added Square

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -8,7 +8,7 @@ canvas.width = width;
 canvas.height = height;
 
 let points = [];
-
+let rects = [];
 
 
 function animate(){
@@ -16,14 +16,24 @@ function animate(){
     requestAnimationFrame(animate);
     let color = "rgb(" + getRandom(255) + "," + getRandom(255) + "," + getRandom(255) + ")"
     let A = new Point(new Vector2d(getRandom(width),getRandom(height)),getRandom(100),color);
+    let B = new Rectangle(new Vector2d(getRandom(width),getRandom(height)),getRandom(100),getRandom(100),color);
     A.draw(context);
+    B.draw(context);
     points.push(A);
+    rects.push(B);
 
     for(let i= 0; i < points.length; i++){
         points[i].radius++;
         points[i].draw(context);
         if(points[i].radius > 200){
             points.splice(i,1);
+        }
+    }
+    for(let i= 0; i < rects.length; i++){
+        rects[i].radius++;
+        rects[i].draw(context);
+        if(rects[i].radius > 200){
+            rects.splice(i,1);
         }
     }
 }
